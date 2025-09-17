bafbebc63b6c3cd455e7ab3c3e8859978cef538e
dawn <jiewenhuang3@gmail.com>
2025-09-17 02:08:16 -0400

    fixed text

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -9,7 +9,7 @@ const About = forwardRef((props, ref) => (
                     Based in Stuyvesant High School, Stuyvesant Science Olympiad has been the largest outlet for students to explore their love in science for over twenty years. Founded by a passionate group of student scientists and engineers, we quickly burst onto the NYC metropolitan scene as a first place team in 2004, staying on top to this day.
                 </p>
                 <p>
-                    <span className="gradient-text font-bold">For the past two years, Stuyvesant has been the best Science Olympiad team in New York City!</span>
+                    <span className="font-bold"> In recent years, Stuyvesant has managed to perform consistently well at the New York State competition, achieving the highest in-person State placing by any NYC team in history. In 2025, Stuyvesant became the first New York City high school to qualify for an in-person National tournament.</span>
                 </p>
                 <p>
                     And competition is just part of what we do. We foster a great community of science-oriented individuals and teach everyone the skills they need to succeed!
